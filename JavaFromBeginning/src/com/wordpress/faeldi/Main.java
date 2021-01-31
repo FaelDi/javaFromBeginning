f6ebcf97cc821e922cb8ffb41316eb6d12449502
rafael.ferreira <rafael.ferreira@geofusion.com.br>
2021-01-30 21:36:38 -0300

    Types

--- JavaFromBeginning/src/com/wordpress/faeldi/Main.java
+++ JavaFromBeginning/src/com/wordpress/faeldi/Main.java
@@ -1,8 +1,65 @@
 package com.wordpress.faeldi;
 
+import java.awt.Point;
+import java.util.Arrays;
+import java.util.Date;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+      /*Types
+      byte age = 30;
+
+      System.out.println("Byte Max Value: "+Byte.MAX_VALUE);
+      System.out.println("Byte Max Value: "+Byte.MIN_VALUE);
+      System.out.println("Integer Max Value: "+Integer.MAX_VALUE);
+      System.out.println("Integer Min Value: "+Integer.MIN_VALUE);
+
+      long viewsCount = 3_123_456_789L;
+
+      System.out.println("Long Max Value: "+Long.MAX_VALUE);
+      System.out.println("Long Min Value: "+Long.MIN_VALUE);
+
+      float price = 10.99F;
+
+      System.out.println("Float Max Value: "+Float.MAX_VALUE);
+      System.out.println("Float Min Value: "+Float.MIN_VALUE);
+      System.out.println("Double Max Value: "+Double.MAX_VALUE);
+      System.out.println("Double Max Value: "+Double.MIN_VALUE);
+
+      char letter = 'R';
+      boolean isElegible = false;
+
+      */
+
+      //Primitive Vs Reference
+//      byte age = 27;
+//      Date now = new Date();
+//      System.out.print(now);
+
+//      Point point1 = new Point(1,1);
+//      Point point2 = point1;
+//
+//      point1.x = 2;
+//      System.out.print(point1);
+
+//      String name = "   Rafael "+"Dias "+"Ferreira"+"!";
+//      System.out.println(name.endsWith("!"));
+//      System.out.println(name.indexOf("!"));
+//      System.out.println(name.replace("!"," *"));
+//      System.out.println(name.trim());
+//      System.out.println(name.toLowerCase());
+//
+//      // \t -> TAB
+//      String message = "c:\\Windows\\....";
+//      System.out.println(message);
+
+      //Arrays
+      int[] numbers = new int[5];
+      numbers[0] = 1;
+      numbers[1] = 4;
+      Arrays.toString(numbers);
+
     }
 }
